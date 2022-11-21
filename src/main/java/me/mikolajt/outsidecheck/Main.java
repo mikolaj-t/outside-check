bc9867621b36a9d091bd036d1f80c71c31122a83
mikolaj-t <51460555+mikolaj-t@users.noreply.github.com>
2022-11-22 00:19:48 +0100

    Basic CLI argument parsing

--- src/main/java/me/mikolajt/outsidecheck/Main.java
+++ src/main/java/me/mikolajt/outsidecheck/Main.java
@@ -2,6 +2,10 @@ package me.mikolajt.outsidecheck;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(String s : args){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Hello ").append(s).append("!");
+            System.out.println(sb);
+        }
     }
 }
