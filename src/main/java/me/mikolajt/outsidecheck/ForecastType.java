e8b6907c0a3afdf37666c955df878d5d6da98c31
mikolaj-t <51460555+mikolaj-t@users.noreply.github.com>
2022-11-24 02:23:44 +0100

    Implement data fetching from OpenWeather API

--- src/main/java/me/mikolajt/outsidecheck/ForecastType.java
+++ src/main/java/me/mikolajt/outsidecheck/ForecastType.java
@@ -2,7 +2,9 @@ package me.mikolajt.outsidecheck;
 
 public enum ForecastType {
     DAILY, HOURLY;
-
+    public static ForecastType theOtherOne(ForecastType type){
+        return ForecastType.values()[(type.ordinal() + 1) % 2];
+    }
     public static ForecastType FromString(String s) throws IllegalArgumentException{
         return ForecastType.valueOf(s.toUpperCase());
     }
--- src/main/java/me/mikolajt/outsidecheck/Main.java
+++ src/main/java/me/mikolajt/outsidecheck/Main.java
@@ -3,10 +3,53 @@ package me.mikolajt.outsidecheck;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    public static void main(String[] args) {
+        if(args.length != 4){
+            printUsage("Argument count mismatch: " + args.length + " vs 4");
+            return;
+        }
+
+        String apiKey = args[0];
+        String configPath = args[1];
+
+        ForecastType forecastType;
+        try {
+            forecastType = ForecastType.FromString(args[2]);
+        } catch (IllegalArgumentException e) {
+            printUsage("Incorrect forecast type:" + args[2]);
+            return;
+        }
+
+        String city = args[3];
+
+        Map<String, CityConfig> config;
+        try {
+            config = readConfigFile(configPath);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        if(!config.containsKey(city.toUpperCase())){
+            System.err.printf("City is not defined in config: " + city);
+            return;
+        }
+
+        var httpClient = HttpClient.newBuilder().build();
+        fetchApi(httpClient, apiKey, config.get(city.toUpperCase()), forecastType);
+    }
+
     private static void printUsage(String contextText){
         if(contextText.length() > 0){
             System.err.println(contextText);
@@ -37,36 +80,20 @@ public class Main {
         return configMap;
     }
 
-    public static void main(String[] args) {
-        if(args.length != 4){
-            printUsage("Argument count mismatch: %d vs 4".formatted(args.length));
-            return;
-        }
-
-        String apiKey = args[0];
-        String configPath = args[1];
-
-        ForecastType forecastType;
+    private static void fetchApi(HttpClient client, String apiKey, CityConfig cityConfig, ForecastType forecastType) {
+        String url = "https://api.openweathermap.org/data/2.5/onecall"
+                + "?lat=" + cityConfig.latitude() + "&lon=" + cityConfig.longitude()
+                + "&exclude=current,minutely,alerts,"
+                + ForecastType.theOtherOne(forecastType).toString().toLowerCase()
+                + "&appid=" + apiKey;
+        System.out.println(url);
         try {
-            forecastType = ForecastType.FromString(args[2]);
-        } catch (IllegalArgumentException e) {
-            printUsage("Incorrect forecast type: %s".formatted(args[2]));
-            return;
-        }
-
-        String city = args[3];
-
-        Map<String, CityConfig> config;
-        try {
-            config = readConfigFile(configPath);
-        } catch (IOException e) {
+            var request = HttpRequest.newBuilder().uri(new URI(url)).GET()
+                    .timeout(Duration.of(10, ChronoUnit.SECONDS)).build();
+            var response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (URISyntaxException | IOException | InterruptedException e) {
             System.err.println(e.getMessage());
-            return;
-        }
-
-        if(!config.containsKey(city.toUpperCase())){
-            System.err.printf("City is not defined in config: %s%n", city);
-            return;
         }
     }
 }
